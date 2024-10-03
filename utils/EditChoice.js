4d9d3da1293b6f9d78ad9b4f5633e48cf1612281
Yamlak1 <yamifikru4@gmail.com>
2024-10-03 13:18:39 +0300

    no property edit fixed

--- utils/EditChoice.js
+++ utils/EditChoice.js
@@ -1,98 +1,154 @@
 async function processEditChoices(ctx) {
-    const { session } = ctx;
+  const { session } = ctx;
 
-    if (!session.isNewUser) {
-        if (!session.propertiesQueue || session.propertiesQueue.length === 0) {
-            ctx.reply("No more properties to edit.");
-            return;
-        }
+  if (!session.isNewUser) {
+    if (!session.propertiesQueue || session.propertiesQueue.length === 0) {
+      const formattedButtons = session.propertiesQueue.map((property) => ({
+        text: property.name,
+        callback_data: `edit_property_${property.id}_${property.name}`,
+      }));
 
-        const formattedButtons = session.propertiesQueue.map((property) => ({
-            text: property.name,
-            callback_data: `edit_property_${property.id}_${property.name}`,
-        }));
+      formattedButtons.push(
+        { text: "Measurement ", callback_data: "edit_metrics" },
+        { text: "Quantity", callback_data: "edit_quantity" }
+      );
 
-        formattedButtons.push({ text: "Measurement ", callback_data: "edit_metrics" }, { text: "Quantity", callback_data: "edit_quantity" });
+      const inlineKeyboard = {
+        reply_markup: {
+          inline_keyboard: chunkArray(formattedButtons, 2), // Split into two columns
+        },
+      };
 
-        const inlineKeyboard = {
-            reply_markup: {
-                inline_keyboard: chunkArray(formattedButtons, 2), // Split into two columns
-            },
-        };
-
-        sentMessage = await ctx.reply(`Choose a property to edit:`, inlineKeyboard);
-        if (sentMessage) {
-            ctx.session.lastMessageId = sentMessage.message_id;
-        }
-    } else {
-        if (!session.propertiesQueue || session.propertiesQueue.length === 0) {
-            ctx.reply("No more properties to edit.");
-            return;
-        }
-
-        const formattedButtons = [
-            ...session.propertiesQueue.map((property) => ({
-                text: property.name,
-                callback_data: `edit_propertyWithuser_${property.id}_${property.name}`,
-            })),
-            { text: "Measurement ", callback_data: "edit_metricsWithUser" },
-            { text: "Quantity", callback_data: "edit_quantityWithUser" },
-            { text: "Full Name", callback_data: "edit_username" },
-            { text: "Phone Number", callback_data: "edit_phoneNumber" },
-            { text: "Business Type", callback_data: "edit_businessType" },
-        ];
-
-        const inlineKeyboard = {
-            reply_markup: {
-                inline_keyboard: chunkArray(formattedButtons, 2), // Split into two columns
-            },
-        };
-
-        sentMessage = await ctx.reply(`Choose a Property to edit:`, inlineKeyboard);
-
-        if (sentMessage) {
-            ctx.session.lastMessageId = sentMessage.message_id;
-        }
+      sentMessage = await ctx.reply(
+        `Choose a property to edit:`,
+        inlineKeyboard
+      );
+      if (sentMessage) {
+        ctx.session.lastMessageId = sentMessage.message_id;
+      }
+      return;
     }
+
+    const formattedButtons = session.propertiesQueue.map((property) => ({
+      text: property.name,
+      callback_data: `edit_property_${property.id}_${property.name}`,
+    }));
+
+    formattedButtons.push(
+      { text: "Measurement ", callback_data: "edit_metrics" },
+      { text: "Quantity", callback_data: "edit_quantity" }
+    );
+
+    const inlineKeyboard = {
+      reply_markup: {
+        inline_keyboard: chunkArray(formattedButtons, 2), // Split into two columns
+      },
+    };
+
+    sentMessage = await ctx.reply(`Choose a property to edit:`, inlineKeyboard);
+    if (sentMessage) {
+      ctx.session.lastMessageId = sentMessage.message_id;
+    }
+  } else {
+    if (!session.propertiesQueue || session.propertiesQueue.length === 0) {
+      const formattedButtons = [
+        ...session.propertiesQueue.map((property) => ({
+          text: property.name,
+          callback_data: `edit_propertyWithuser_${property.id}_${property.name}`,
+        })),
+        { text: "Measurement ", callback_data: "edit_metricsWithUser" },
+        { text: "Quantity", callback_data: "edit_quantityWithUser" },
+        { text: "Full Name", callback_data: "edit_username" },
+        { text: "Phone Number", callback_data: "edit_phoneNumber" },
+        { text: "Business Type", callback_data: "edit_businessType" },
+      ];
+
+      const inlineKeyboard = {
+        reply_markup: {
+          inline_keyboard: chunkArray(formattedButtons, 2), // Split into two columns
+        },
+      };
+
+      sentMessage = await ctx.reply(
+        `Choose a Property to edit:`,
+        inlineKeyboard
+      );
+
+      if (sentMessage) {
+        ctx.session.lastMessageId = sentMessage.message_id;
+      }
+      return;
+    }
+
+    const formattedButtons = [
+      ...session.propertiesQueue.map((property) => ({
+        text: property.name,
+        callback_data: `edit_propertyWithuser_${property.id}_${property.name}`,
+      })),
+      { text: "Measurement ", callback_data: "edit_metricsWithUser" },
+      { text: "Quantity", callback_data: "edit_quantityWithUser" },
+      { text: "Full Name", callback_data: "edit_username" },
+      { text: "Phone Number", callback_data: "edit_phoneNumber" },
+      { text: "Business Type", callback_data: "edit_businessType" },
+    ];
+
+    const inlineKeyboard = {
+      reply_markup: {
+        inline_keyboard: chunkArray(formattedButtons, 2), // Split into two columns
+      },
+    };
+
+    sentMessage = await ctx.reply(`Choose a Property to edit:`, inlineKeyboard);
+
+    if (sentMessage) {
+      ctx.session.lastMessageId = sentMessage.message_id;
+    }
+  }
 }
 
 // Function to chunk array into two-dimensional arrays for two columns
 function chunkArray(array, size) {
-    const chunkedArr = [];
-    for (let i = 0; i < array.length; i += size) {
-        chunkedArr.push(array.slice(i, i + size));
-    }
-    return chunkedArr;
+  const chunkedArr = [];
+  for (let i = 0; i < array.length; i += size) {
+    chunkedArr.push(array.slice(i, i + size));
+  }
+  return chunkedArr;
 }
 
 async function EditUser(ctx) {
-    const inlineKeyboard = {
-        reply_markup: {
-            inline_keyboard: [
-                [{
-                    text: "Edit Username",
-                    callback_data: "edit_username_to_viewContact",
-                }, ],
-                [{
-                    text: "Edit Phone Number",
-                    callback_data: "edit_phoneNumber_to_viewContact",
-                }, ],
-                [{
-                    text: "Edit Business Type",
-                    callback_data: "edit_businessType_to_viewContact",
-                }, ],
-            ],
-        },
-    };
+  const inlineKeyboard = {
+    reply_markup: {
+      inline_keyboard: [
+        [
+          {
+            text: "Edit Username",
+            callback_data: "edit_username_to_viewContact",
+          },
+        ],
+        [
+          {
+            text: "Edit Phone Number",
+            callback_data: "edit_phoneNumber_to_viewContact",
+          },
+        ],
+        [
+          {
+            text: "Edit Business Type",
+            callback_data: "edit_businessType_to_viewContact",
+          },
+        ],
+      ],
+    },
+  };
 
-    sentMessage = await ctx.reply(
-        "Choose what you want to edit:",
-        inlineKeyboard
-    );
+  sentMessage = await ctx.reply(
+    "Choose what you want to edit:",
+    inlineKeyboard
+  );
 
-    if (sentMessage) {
-        ctx.session.lastMessageId = sentMessage.message_id;
-    }
+  if (sentMessage) {
+    ctx.session.lastMessageId = sentMessage.message_id;
+  }
 }
 
 module.exports = { processEditChoices, EditUser };
